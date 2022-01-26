74a3d0c8785fcf3424dee5cf74f7c054fec52e08
Simon Busch <simon@busch.pro>
2022-01-26 23:36:29 +0000

    -only to get into cloud

--- src/pong.cpp
+++ src/pong.cpp
@@ -9,11 +9,14 @@
 #define PONG_OFFSET 1
 #define PONG_REGULAR 0
 #define PONG_DEBUG 1
-#define PONG_RATIO (float)(4.0f / 3.0f)
-#define PONG_COSYS_XMAX 11
-#define PONG_COSYS_YMAX 8
+#define PONG_RATIO (float)(21.0f / 11.0f)
+#define PONG_COSYS_XMAX 20
+#define PONG_COSYS_YMAX 10
+#define PONG_COSYS_WIDTH 21
+#define PONG_COSYS_HEIGHT 11
 #define DEBUG_CPAIRRED 1
 #define DEBUG_CPAIRBLUE 2
+#define PONG_SLIDEROFFSET 2
 
 typedef enum input
 {
@@ -30,7 +33,7 @@ typedef enum input
 
 typedef struct frame
 {
-    char** pxlbuf;
+    char pxlbuf[PONG_COSYS_HEIGHT][PONG_COSYS_WIDTH];
 } frame;
 
 typedef struct point
@@ -49,8 +52,8 @@ typedef struct gamestate
 {
     bool alive;
     int condition;
-    int p1pos;
-    int p2pos;
+    int p1pos, p1points;
+    int p2pos, p2points;
     vector ballvec;
     point ballpos;
     input inbuf;
@@ -66,12 +69,112 @@ typedef enum error
 } error;
 
 gamestate init_game(int condition);
-frame create_frame();
-error free_frame(frame);
 error render(gamestate);
 error run(gamestate*);
 input get_input();
 void cleanup();
+error push_frame(frame);
+point create_point(int, int);
+vector create_vector(point, point);
+error prepare_frame(frame*);
+
+gamestate init_game(int condition)
+{
+    {
+        initscr();
+        timeout(0);
+        curs_set(0);
+        noecho();
+        start_color();
+        init_pair(DEBUG_CPAIRBLUE, COLOR_BLUE, COLOR_BLACK);
+        init_pair(DEBUG_CPAIRRED, COLOR_RED, COLOR_BLACK);
+    }
+
+    gamestate _return;
+
+    // Prints uppper border
+    {
+        mvaddch(0, 0, 'x');
+        for(int i = 0; i < PONG_COSYS_WIDTH * 2 + PONG_OFFSET; i++)
+            addch('-');
+        addch('x');
+    }
+
+    // prints lower border
+    {
+        mvaddch(PONG_COSYS_HEIGHT + PONG_OFFSET, 0, 'x');
+        for(int i = 0; i < PONG_COSYS_WIDTH * 2 + PONG_OFFSET; i++)
+            addch('-');
+        addch('x'); 
+    }
+
+    // prints side borders
+    for(int i = 0; i < PONG_COSYS_HEIGHT; i++)
+    {
+        mvaddch(i + PONG_OFFSET, 0, '|');
+        mvaddch(i + PONG_OFFSET, PONG_COSYS_WIDTH * 2 + PONG_OFFSET * 2, '|');
+    }
+
+    // Prints headline + intial points position
+    {
+        const char* name = " Pongy ";
+        mvprintw(0, (PONG_COSYS_WIDTH - (cround((float)strlen(name) / 2.0f) - PONG_OFFSET - 1)), name);
+
+        mvprintw(0, cround((float)PONG_COSYS_HEIGHT / 2.0f) - 1, " %d ", 0);
+        mvprintw(0, PONG_COSYS_HEIGHT + cround((float)PONG_COSYS_HEIGHT / 2.0f) + 1, " %d ", 0);
+    }
+
+    // prints various debug info
+    if(condition == PONG_DEBUG)
+    {
+        mvprintw
+        (
+            0, 0,
+            "h:%d w:%d t: %d e: %f r: %f, my: %d",
+            PONG_COSYS_HEIGHT, PONG_COSYS_WIDTH, PONG_COSYS_HEIGHT * PONG_COSYS_WIDTH, (float)((float)PONG_COSYS_HEIGHT * (float)PONG_RATIO), PONG_RATIO, cround((float)PONG_COSYS_WIDTH / 2.0f) + PONG_OFFSET
+        );
+    }
+
+    // copies calculated data into our return obj
+    _return.p1pos = 1;
+    _return.p2pos = PONG_COSYS_YMAX - PONG_SLIDEROFFSET - 1;
+    _return.framecount = 0;
+    _return.ballpos = create_point(cround((float)PONG_COSYS_XMAX / 2.0f), cround((float)PONG_COSYS_YMAX / 2.0f));
+    _return.condition = condition;
+    prepare_frame(&_return.framebuffer);
+
+    // seeds rand function
+    srand(time(NULL));
+
+    // creates random between 0, 1
+    bool random = rand() % 2;
+
+    // calculates random ball vector
+    _return.ballvec = create_vector
+    (
+        create_point(rand() % PONG_COSYS_WIDTH, rand() % PONG_COSYS_HEIGHT),
+        _return.ballpos
+    );
+
+    // defines gamestate as alive
+    _return.alive = true;
+
+    // returns
+    return _return;
+}
+
+error prepare_frame(frame* fptr)
+{
+    for(int y = 0; y < PONG_COSYS_HEIGHT; y++)
+    {
+        for(int x = 0; x < PONG_COSYS_WIDTH; x++)
+        {
+            fptr->pxlbuf[y][x] = ' ';
+        }
+    }
+
+    return NOERR;
+}
 
 point create_point(int x, int y)
 {
@@ -91,141 +194,17 @@ vector create_vector(point dest, point orig)
     return _return;
 }
 
-gamestate init_game(int condition)
+error push_frame(frame f)
 {
+    for(int y = 0; y < PONG_COSYS_HEIGHT; y++)
     {
-        initscr();
-        timeout(0);
-        curs_set(0);
-        noecho();
-        start_color();
-
-        init_pair(DEBUG_CPAIRBLUE, COLOR_BLUE, COLOR_BLACK);
-        init_pair(DEBUG_CPAIRRED, COLOR_RED, COLOR_BLACK);
-    }
-
-    gamestate _return;
-
-    // Prints uppper border
-    {
-        mvaddch(0, 0, 'x');
-        for(int i = 0; i < PONG_COSYS_XMAX * 2 + PONG_OFFSET; i++)
-            addch('-');
-        addch('x');
-    }
-
-    // Prints y debug center
-    if(condition == PONG_DEBUG)
-    {
-        for(int i = 1; i < PONG_COSYS_XMAX * 2; i++)
+        for(int x = 0; x < PONG_COSYS_WIDTH; x++)
         {
-            attron(COLOR_PAIR(DEBUG_CPAIRRED));
-            mvaddch(cround((float)PONG_COSYS_YMAX / 2.0f) + PONG_OFFSET, 0, ' ');
-            for(int i = 0; i < PONG_COSYS_XMAX * 2 + PONG_OFFSET; i++)
-                addch('-');
-            attroff(COLOR_PAIR(DEBUG_CPAIRRED));
+            mvaddch(y + PONG_OFFSET, (x * 2) + PONG_OFFSET * 2, f.pxlbuf[y][x]);
         }
     }
 
-    // prints lower border
-    {
-        mvaddch(PONG_COSYS_YMAX + PONG_OFFSET, 0, 'x');
-        for(int i = 0; i < PONG_COSYS_XMAX * 2 + PONG_OFFSET; i++)
-            addch('-');
-        addch('x'); 
-    }
-
-    // prints side borders
-    for(int i = 0; i < PONG_COSYS_YMAX; i++)
-    {
-        mvaddch(i + PONG_OFFSET, 0, '|');
-
-        // prints y center border
-        if(condition == PONG_DEBUG)
-        {
-            attron(COLOR_PAIR(DEBUG_CPAIRBLUE));
-            mvaddch(i + PONG_OFFSET, cround((PONG_COSYS_XMAX / 2.0f) / PONG_COSYS_XMAX * PONG_COSYS_XMAX * 2 + PONG_OFFSET), '|');
-            attroff(COLOR_PAIR(DEBUG_CPAIRBLUE));
-        }
-
-        mvaddch(i + PONG_OFFSET, PONG_COSYS_XMAX * 2 + PONG_OFFSET * 2, '|');
-    }
-
-    // Prints headline + intial points position
-    {
-        const char* name = " Pongy ";
-        mvprintw(0, (PONG_COSYS_XMAX - (cround((float)strlen(name) / 2.0f) - PONG_OFFSET - 1)), name);
-
-        mvprintw(0, cround((float)PONG_COSYS_XMAX / 2.0f) - 1, " %d ", 0);
-        mvprintw(0, PONG_COSYS_XMAX + cround((float)PONG_COSYS_XMAX / 2.0f) + 1, " %d ", 0);
-    }
-
-    // prints various debug info
-    if(condition == PONG_DEBUG)
-    {
-        mvprintw
-        (
-            0, 0,
-            "h:%d w:%d t: %d e: %f r: %f, my: %d",
-            PONG_COSYS_YMAX, PONG_COSYS_XMAX, PONG_COSYS_YMAX * PONG_COSYS_XMAX, (float)((float)PONG_COSYS_YMAX * (float)PONG_RATIO), PONG_RATIO, cround((float)PONG_COSYS_YMAX / 2.0f) + PONG_OFFSET
-        );
-    }
-
-    // creates initial frame
-    frame f = create_frame();
-
-    // copies calculated data into our return obj
-    _return.p1pos = 1;
-    _return.p2pos = 6;
-    _return.framecount = 0;
-    _return.framebuffer = f;
-    _return.ballpos = create_point(cround((float)PONG_COSYS_XMAX / 2.0f), cround((float)PONG_COSYS_YMAX / 2.0f));
-    _return.condition = condition;
-
-    // seeds rand function
-    time_t *tptr;
-    time(tptr);
-    rand_r((unsigned int*)tptr);
-
-    // creates random between 0, 1
-    bool random = rand() % 2;
-
-    // calculates random ball vector
-    _return.ballvec = create_vector
-    (
-        create_point(rand() % PONG_COSYS_XMAX, rand() % PONG_COSYS_YMAX),
-        _return.ballpos
-    );
-
-    // defines gamestate as alive
-    _return.alive = true;
-
-    // returns
-    return _return;
-}
-
-frame create_frame()
-{
-    frame _return;
-
-    // allocates memory
-    _return.pxlbuf = (char**)malloc(PONG_COSYS_YMAX * sizeof(char*));
-    for(int i = 0; i < PONG_COSYS_YMAX; i++)
-        _return.pxlbuf[i] = (char*)malloc(PONG_COSYS_XMAX * sizeof(char));
-
-    // fills memory with ' ' chars
-    for(int i = 0; i < PONG_COSYS_YMAX; i++)
-        for(int j = 0; j < PONG_COSYS_XMAX; j++)
-            _return.pxlbuf[i][j] =  ' ';
-
-    return _return;
-}
-
-// not correctly implemented. Do Not Use!
-error free_frame(frame f)
-{
-    for(int i = 0; i < PONG_COSYS_YMAX; i++)
-        free(f.pxlbuf[i]);
+    refresh();
 
     return NOERR;
 }
@@ -253,67 +232,37 @@ error render(gamestate gs)
 {
     // Ball computation
     {
-        mvaddch(gs.ballpos.y + PONG_OFFSET, gs.ballpos.x * 2 + PONG_OFFSET, '@');
+        gs.framebuffer.pxlbuf[gs.ballpos.y][gs.ballpos.x] = '@';
     }
 
     // p1 slider
     {
-        for(int i = 0; i < PONG_COSYS_YMAX; i++)
-            mvaddch(i + PONG_OFFSET, 0 + PONG_OFFSET, ' ');
-        for(int i = 0; i < 2; i++)
-            mvaddch(gs.p1pos + PONG_OFFSET + i, 0 + PONG_OFFSET, '|');
+        for(int i = 0; i < PONG_COSYS_HEIGHT; i++)
+            gs.framebuffer.pxlbuf[i][0] = ' ';
+        for(int i = 0; i <= PONG_SLIDEROFFSET; i++)
+            gs.framebuffer.pxlbuf[gs.p1pos + i][0] = '|';
     }
 
     // p2 slider
     {
 
-        for(int i = 0; i < PONG_COSYS_YMAX; i++)
-            mvaddch(i + PONG_OFFSET, PONG_COSYS_XMAX * 2 + PONG_OFFSET, ' ');
-        for(int i = 0; i < 2; i++)
-            mvaddch(gs.p2pos + PONG_OFFSET + i, PONG_COSYS_XMAX * 2 + PONG_OFFSET, '|');
+        for(int i = 0; i < PONG_COSYS_HEIGHT; i++)
+            gs.framebuffer.pxlbuf[i][PONG_COSYS_XMAX] = ' ';
+        for(int i = 0; i <= PONG_SLIDEROFFSET; i++)
+            gs.framebuffer.pxlbuf[gs.p2pos + i][PONG_COSYS_XMAX] = '|';
     }
 
-    // debug computation
-    if(gs.condition == PONG_DEBUG)
+    if(gs.condition = PONG_DEBUG)
     {
-        int desty;
-        int destx;
-
-        /* vector computation
-        {
-            desty = cround((float)gs.ballvec.destination.y / PONG_COSYS_YMAX * PONG_COSYS_YMAX); 
-            destx = cround((float)gs.ballvec.destination.x / PONG_COSYS_XMAX * PONG_COSYS_XMAX * 2 + PONG_OFFSET);
-
-            mvaddch(desty + PONG_OFFSET, destx, 'O');
-
-            mvprintw(gs.framebuffer.PONG_COSYS_YMAX + 4, 0, "O: ");
-
-            mvprintw
-            (
-                gs.framebuffer.PONG_COSYS_YMAX + 4, 3,
-                "y: (%f / %f) * %d = %d",
-                gs.ballvec.destination.y, PONG_COSYS_YMAX, gs.framebuffer.PONG_COSYS_YMAX, desty
-            );
-
-
-            mvprintw
-            (
-                gs.framebuffer.PONG_COSYS_YMAX + 5, 3,
-                "x: (%f / %f) * %d = %d",
-                gs.ballvec.destination.x, PONG_COSYS_XMAX, gs.framebuffer.PONG_COSYS_XMAX, destx
-            );
-        }*/
-
-        // prints framecount
         mvprintw
         (
-            PONG_COSYS_YMAX + 6, 0,
+            PONG_COSYS_YMAX + PONG_OFFSET * 2, 0,
             "framecount: %d",
             gs.framecount
         );
     }
 
-    refresh();
+    push_frame(gs.framebuffer);
 
     return NOERR;
 }
@@ -349,9 +298,9 @@ error run(gamestate* gsptr)
         // s pressed -> moves slider p1 up
         case DOWN_P1:
         {
-            if(gsptr->p1pos >= 6)
+            if(gsptr->p1pos >= PONG_COSYS_YMAX - PONG_SLIDEROFFSET)
             {
-                gsptr->p1pos = 6;
+                gsptr->p1pos = PONG_COSYS_YMAX - PONG_SLIDEROFFSET;
             }
             else
             {
@@ -377,9 +326,9 @@ error run(gamestate* gsptr)
         // k pressed -> moves slider p2 down
         case DOWN_P2:
         {
-            if(gsptr->p2pos >= 6)
+            if(gsptr->p2pos >= PONG_COSYS_YMAX - PONG_SLIDEROFFSET)
             {
-                gsptr->p2pos = 6;
+                gsptr->p2pos = PONG_COSYS_YMAX - PONG_SLIDEROFFSET;
             }
             else
             {
@@ -387,6 +336,33 @@ error run(gamestate* gsptr)
             }
         }
         break;
+    }
+
+    // evaluate if ballpos has left the coordinate system on x
+    if(gsptr->ballpos.x > PONG_COSYS_XMAX)
+    {
+        gsptr->p1points++;
+
+        // creates new ball vector & position
+        gsptr->ballpos = create_point(cround((float)PONG_COSYS_XMAX / 2.0f), cround((float)PONG_COSYS_YMAX / 2.0f));
+        gsptr->ballvec = create_vector
+        (
+            create_point(rand() % PONG_COSYS_XMAX, rand() % PONG_COSYS_YMAX),
+            gsptr->ballpos
+        );
+    }
+
+    else if(gsptr->ballpos.x < 0)
+    {
+        gsptr->p2points++;
+
+        // creates new ball vector & position
+        gsptr->ballpos = create_point(cround((float)PONG_COSYS_XMAX / 2.0f), cround((float)PONG_COSYS_YMAX / 2.0f));
+        gsptr->ballvec = create_vector
+        (
+            create_point(rand() % PONG_COSYS_XMAX, rand() % PONG_COSYS_YMAX),
+            gsptr->ballpos
+        );
     }
 
     return NOERR;
@@ -430,4 +406,6 @@ int main(int argc, char** argv)
     }   // exists when gs.alive is false
 
     cleanup();
+
+    return 0;
 }
